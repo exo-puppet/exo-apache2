4b68a92a45a2bcf1375c44397ed768f2d38f90f8
Frederic DROUET <fdrouet@exoplatform.com>
2012-02-13 17:04:09 +0100

    (new) : create puppet module for Apache 2 wsgi module

---
+++ manifests/mod/wsgi.pp
@@ -0,0 +1,8 @@
+class apache2::mod::wsgi ( $activated=true ) {
+    apache2::module { "wsgi":
+        activated       => $activated,
+        package_name    => "libapache2-mod-wsgi",
+        conf_file       => true,
+        require         => Package [ "httpd" ],
+    }
+}
