2948c9878077e2cb998f06d04eefa12fa1ab3d59
Frederic DROUET <fdrouet@exoplatform.com>
2012-03-30 17:29:11 +0200

    (new) Apache2 python module is now manage by puppet

---
+++ manifests/mod/python.pp
@@ -0,0 +1,8 @@
+class apache2::mod::python ( $activated=true ) {
+    apache2::module { "python":
+        activated       => $activated,
+        package_name    => "libapache2-mod-python",
+        conf_file       => false,
+        require         => Package [ "httpd" ],
+    }
+}
