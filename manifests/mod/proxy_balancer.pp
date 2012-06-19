3e4b7ebaea1cf65c32d5cdfed3ceb4efaa0a4365
Frederic DROUET <fdrouet@exoplatform.com>
2012-06-19 22:11:51 +0100

    tqa-inj01 is now well configured to load balance all 3 tqa-asXX nodes

--- manifests/mod/proxy_balancer.pp
+++ manifests/mod/proxy_balancer.pp
@@ -1,8 +1,8 @@
-class apache2::mod::proxy_balancer ( $activated=true, $balancer_status=true, $balancer_status_show_all=false ) {
+class apache2::mod::proxy_balancer ( $activated=true ) {
     apache2::module { "proxy_balancer":
         activated           => $activated,
         conf_file           => true,
-        conf_file_template  => true,
+        conf_file_template  => false,
         require             => [ Package [ "httpd" ], Class ["apache2::mod::status", "apache2::mod::proxy"] ]
     }
 }
