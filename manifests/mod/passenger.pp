f0349e0bf3b6f9e1f2bf6c09df77d01687bb53ea
Frederic DROUET <fdrouet@exoplatform.com>
2013-02-19 23:12:39 +0100

    Apache 2 mod passenger must declare a package name

--- manifests/mod/passenger.pp
+++ manifests/mod/passenger.pp
@@ -1,6 +1,7 @@
 class apache2::mod::passenger ( $activated=true ) {
     apache2::module { "passenger":
         activated           => $activated,
+        package_name        => "libapache2-mod-passenger",
         conf_file           => true,
         conf_file_template  => true,
         require             => [ Package [ "httpd" ] ],
