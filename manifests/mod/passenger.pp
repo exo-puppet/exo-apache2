39449b37b5b4c342579c75143facc98e204a4eff
Frédéric DROUET <fdrouet@exoplatform.com>
2016-05-20 14:48:32 +0200

    Upgrade passenger

--- manifests/mod/passenger.pp
+++ manifests/mod/passenger.pp
@@ -1,11 +1,18 @@
 class apache2::mod::passenger (
   $activated = true) {
+  apt::source { 'passenger':
+    location => 'https://oss-binaries.phusionpassenger.com/apt/passenger',
+    key      => '16378A33A6EF16762922526E561F9B9CAC40B2F7',
+    repos    => 'main',
+    release  => "${lsbdistcodename}",
+    require  => Package['apt-transport-https','ca-certificates'],
+  }
+
   apache2::module { 'passenger':
     activated          => $activated,
     package_name       => 'libapache2-mod-passenger',
     conf_file          => true,
     conf_file_template => true,
-    require            => [
-      Package['httpd']],
+    require            => [Package['httpd'],Apt::Source['passenger']],
   }
 }
